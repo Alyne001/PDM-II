78125c4b083b827f121ca9a65c7e5e334001dfd7
Alyne001 <alyne.souza62@aluno.ifce.edu.br>
2025-11-27 09:53:36 -0300

    Atividade 7

---
+++ Avaliacoes/Atividade07/github_client/lib/github_oauth_credentials.dart
@@ -0,0 +1,5 @@
+// TODO(CodelabUser): Create an OAuth App
+const githubClientId = 'Ov23livrqLLRIRXsUz7T';
+const githubClientSecret = 'ca8b8ba085a452349f4a12029e8a2d1235899d55';
+
+const githubScopes = ['repo', 'read:org'];
